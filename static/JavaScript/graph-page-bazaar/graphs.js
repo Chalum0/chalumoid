8f0c0f4d11619cc0443823670420ebe1cf41fb59
Snappyink2 <raphaelzanatta24@proton.me>
2024-01-12 11:45:42 +0100

    Added functionnal buy and sell buttons that create the stored graph

--- static/JavaScript/graph-page-bazaar/graphs.js
+++ static/JavaScript/graph-page-bazaar/graphs.js
@@ -1,13 +1,46 @@
 const item = toUpperCaseWithUnderscores(document.querySelector("h1").textContent)
 let amounts = JSON.parse(localStorage.getItem("amounts"))
+console.log(amounts)
 if (amounts === null) {
     amounts = {
+
     }
+    amounts[item] = {values: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], bottomValues: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
 }
 
-amounts[item] = {values: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], bottomValues: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
 
-console.log(amounts)
+
+const buyButton = document.querySelector("#buy-button")
+const sellButton = document.querySelector("#sell-button")
+function canBeConvertedToInt(str) {
+    let num = parseInt(str);
+    return !isNaN(num);
+
+}
+
+buyButton.addEventListener("click", ()=>{
+    let amount = document.querySelector("#quantity").value
+    if (amount !== "" && canBeConvertedToInt(amount)){
+        amounts[item].values.push(
+            parseFloat(amounts[item].values[amounts[item].values.length - 1]) + parseFloat(amount)
+        )
+        amounts[item].values.shift()
+        createChart(amounts[item].values, amounts[item].bottomValues, "Stock", document.querySelector("#graph2"))
+        localStorage.setItem("amounts", JSON.stringify(amounts))
+    }
+})
+
+sellButton.addEventListener("click", ()=>{
+    let amount = document.querySelector("#quantity").value
+    if (amount !== "" && canBeConvertedToInt(amount)){
+        amounts[item].values.push(
+            parseFloat(amounts[item].values[amounts[item].values.length - 1]) - parseFloat(amount)
+        )
+        amounts[item].values.shift()
+        createChart(amounts[item].values, amounts[item].bottomValues, "Stock", document.querySelector("#graph2"))
+        localStorage.setItem("amounts", JSON.stringify(amounts))
+    }
+})
 
 // localStorage.setItem("amounts", JSON.stringify(amounts))
 
@@ -120,39 +153,46 @@ function toUpperCaseWithUnderscores(str) {
 createChart(amounts[item].values, amounts[item].bottomValues, "Stock", document.querySelector("#graph2"))
 
 
+function talkToAPI(){
+    axios.get(`http://chalumoid.fr/projects/bazaar-tracker/items/${item}/api`)
+        .then(function (response) {
+            const apiData = response.data
 
-axios.get(`http://chalumoid.fr/projects/bazaar-tracker/items/${item}/api`)
-    .then(function (response) {
-        const apiData = response.data
-
-        // --CREATE GRAPH FROM API DATA
-        let graphData = apiData //apiData.graph
-        create_chart(graphData.graphDatas[0][0], graphData.graphDatas[0][1], graphData.graphDatas[0][2], graphData.graphBottomData[0], document.querySelector("#graph"))
-
-        // --CREATE TABLE FROM API DATA
-        // const table = document.querySelector("table")
-        // let tableData = apiData.table
-        // for (let v of tableData.buy) {
-        //     let td1 = document.createElement("td")
-        //     td1.textContent = v[0]
-        //     let td2 = document.createElement("td")
-        //     td2.textContent = v[1]
-        //
-        //     table.appendChild(document.createElement("tr").appendChild(td1).appendChild(td2))
-        // }
+            // --CREATE GRAPH FROM API DATA
+            let graphData = apiData //apiData.graph
+            create_chart(graphData.graphDatas[0][0], graphData.graphDatas[0][1], graphData.graphDatas[0][2], graphData.graphBottomData[0], document.querySelector("#graph"))
 
 
 
+            // --CREATE TABLE FROM API DATA
+            // const table = document.querySelector("table")
+            // let tableData = apiData.table
+            // for (let v of tableData.buy) {
+            //     let td1 = document.createElement("td")
+            //     td1.textContent = v[0]
+            //     let td2 = document.createElement("td")
+            //     td2.textContent = v[1]
+            //
+            //     table.appendChild(document.createElement("tr").appendChild(td1).appendChild(td2))
+            // }
 
-    })
-    .catch(function (error) {
-        // en cas d’échec de la requête
-        console.log(error);
-        const canva = document.querySelector("#graph");
-        const wrapper = document.createElement("a");
-        wrapper.id = "apiErrorText"
-        canva.parentNode.insertBefore(wrapper, canva);
-        wrapper.parentNode.style.height = "75%"
-        canva.remove();
-        wrapper.textContent = "Error loading data."
-    })
+
+
+
+        })
+        .catch(function (error) {
+            // en cas d’échec de la requête
+            console.log(error);
+            const canva = document.querySelector("#graph");
+            const wrapper = document.createElement("a");
+            wrapper.id = "apiErrorText"
+            canva.parentNode.insertBefore(wrapper, canva);
+            wrapper.parentNode.style.height = "75%"
+            canva.remove();
+            wrapper.textContent = "Error loading data."
+        })
+}
+
+talkToAPI()
+setInterval(talkToAPI(), 30*60*1000)
+
